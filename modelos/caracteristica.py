8bc4b8009beaa867969a9f6df7f564861156bf7e
Crosscb07 <crosscb07@gmail.com>
2026-08-16 12:30:01 -0300

    Se juntaron los archivos modularizados en una carpeta: main

--- modelos/caracteristica.py
+++
@@ -1,7 +0,0 @@
-class caracteristica:
-  def __init__(self, id, nombre):
-    self.id = id
-    self.nombre = nombre
-  def mostrar_todo(self):
-    print(f"Id: {self.id}")
-    print(f"Nombre: {self.nombre}")

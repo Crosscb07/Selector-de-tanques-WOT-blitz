115cb0589fe45a41966424b2ae1f6bb5763f5195
Crosscb07 <crosscb07@gmail.com>
2026-08-21 22:08:40 -0300

    Cambio en la estructura: Se crearon las tablas de nación, clase, nivel, tipo. Y en la tabla de tanques se las reemplazo por ids

--- selector_de_tanques_wotblitz.sql
+++ selector_de_tanques_wotblitz.sql
@@ -1,6 +1,5 @@
--- Habría que crear nuevas tablas para las clases, niveles, tipos y naciones, y cambiar los valores de las columnas de la tabla de tanques
-
-
+CREATE DATABASE  IF NOT EXISTS `selector_de_tanques_wotblitz` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `selector_de_tanques_wotblitz`;
 -- MySQL dump 10.13  Distrib 8.0.45, for macos15 (x86_64)
 --
 -- Host: localhost    Database: selector_de_tanques_wotblitz
@@ -24,7 +23,7 @@ SET @@SESSION.SQL_LOG_BIN= 0;
 -- GTID state at the beginning of the backup 
 --
 
-SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ 'fb43871a-23db-11f1-9dcc-276fb84d57cf:1-202';
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ 'fb43871a-23db-11f1-9dcc-276fb84d57cf:1-234';
 
 --
 -- Table structure for table `caracteristicas`
@@ -46,8 +45,83 @@ CREATE TABLE `caracteristicas` (
 
 LOCK TABLES `caracteristicas` WRITE;
 /*!40000 ALTER TABLE `caracteristicas` DISABLE KEYS */;
-INSERT INTO `caracteristicas` VALUES (1,'Cañón convencional'),(2,'Cargador automático'),(3,'Recargador automático'),(4,'Daño por minuto'),(5,'Daño por disparo'),(6,'Penetración'),(7,'Tiempo de apuntado'),(8,'Dispersión'),(9,'Depresión del cañón'),(10,'Velocidad máxima'),(11,'Relación potencia/peso'),(12,'Puntos de vida'),(13,'Alcance de visión'),(14,'Camuflaje'),(15,'Blindaje de la torreta'),(16,'Blindaje del casco');
+INSERT INTO `caracteristicas` VALUES (1,'Cañón estándar'),(2,'Autocargador'),(3,'Autorrecargador'),(4,'Daño por minuto'),(5,'Daño por disparo'),(6,'Penetración'),(7,'Tiempo de apuntado'),(8,'Dispersión'),(9,'Depresión del cañón'),(10,'Velocidad máxima'),(11,'Relación potencia / peso'),(12,'Puntos de vida'),(13,'Alcance de visión'),(14,'Camuflaje'),(15,'Blindaje de la torreta'),(16,'Blindaje del casco');
 /*!40000 ALTER TABLE `caracteristicas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clases`
+--
+
+DROP TABLE IF EXISTS `clases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clases` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `clase` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clases`
+--
+
+LOCK TABLES `clases` WRITE;
+/*!40000 ALTER TABLE `clases` DISABLE KEYS */;
+INSERT INTO `clases` VALUES (1,'Ligero'),(2,'Mediano'),(3,'Pesado'),(4,'Cazacarros');
+/*!40000 ALTER TABLE `clases` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `naciones`
+--
+
+DROP TABLE IF EXISTS `naciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `naciones` (
+  `id` int NOT NULL,
+  `nacion` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `naciones`
+--
+
+LOCK TABLES `naciones` WRITE;
+/*!40000 ALTER TABLE `naciones` DISABLE KEYS */;
+INSERT INTO `naciones` VALUES (1,'Estados Unidos'),(2,'Alemania'),(3,'Unión Soviética'),(4,'Reino Unido'),(5,'Japón'),(6,'China'),(7,'Francia'),(8,'Nación Europea'),(9,'Nación Híbrida');
+/*!40000 ALTER TABLE `naciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `niveles`
+--
+
+DROP TABLE IF EXISTS `niveles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `niveles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nivel` varchar(4) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `niveles`
+--
+
+LOCK TABLES `niveles` WRITE;
+/*!40000 ALTER TABLE `niveles` DISABLE KEYS */;
+INSERT INTO `niveles` VALUES (1,'I'),(2,'II'),(3,'III'),(4,'IV'),(5,'V'),(6,'VI'),(7,'VII'),(8,'VIII'),(9,'IX'),(10,'X');
+/*!40000 ALTER TABLE `niveles` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -59,12 +133,22 @@ DROP TABLE IF EXISTS `tanques`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tanques` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) DEFAULT NULL,
-  `nacion` varchar(15) DEFAULT NULL,
-  `clase` varchar(10) DEFAULT NULL,
-  `tier` varchar(4) DEFAULT NULL,
-  `tipo` varchar(17) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `nombre` varchar(100) NOT NULL,
+  `nacion` int NOT NULL,
+  `clase` int NOT NULL,
+  `nivel` int NOT NULL,
+  `tipo` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `nacion_idx` (`nacion`),
+  KEY `clase_idx` (`clase`),
+  KEY `nivel_idx` (`nivel`),
+  KEY `tipo_idx` (`tipo`),
+  CONSTRAINT `clase` FOREIGN KEY (`clase`) REFERENCES `clases` (`id`),
+  CONSTRAINT `nacion` FOREIGN KEY (`nacion`) REFERENCES `naciones` (`id`),
+  CONSTRAINT `nivel` FOREIGN KEY (`nivel`) REFERENCES `niveles` (`id`),
+  CONSTRAINT `tipo` FOREIGN KEY (`tipo`) REFERENCES `tipos` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=64066 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -74,7 +158,7 @@ CREATE TABLE `tanques` (
 
 LOCK TABLES `tanques` WRITE;
 /*!40000 ALTER TABLE `tanques` DISABLE KEYS */;
-INSERT INTO `tanques` VALUES (10033,'WZ-132A','China','Ligero','IX','Árbol tecnológico'),(20257,'Sheridan','Estados Unidos','Ligero','X','Árbol tecnológico'),(22545,'Kanonenjagdpanzer','Alemania','Destructor','IX','Coleccionista'),(28689,'Rhm. Pzw.','Alemania','Ligero','X','Árbol tecnológico'),(64065,'FCM 50 t','Francia','Medio','VIII','Coleccionista');
+INSERT INTO `tanques` VALUES (10033,'WZ-132A',6,1,9,1),(20257,'Sheridan',1,1,10,1),(22545,'Kanonenjagdpanzer',2,4,9,3),(28689,'Rhm. Pzw.',2,1,10,1),(64065,'FCM 50 t',7,2,8,3);
 /*!40000 ALTER TABLE `tanques` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -104,6 +188,31 @@ LOCK TABLES `tanques_caracteristicas` WRITE;
 INSERT INTO `tanques_caracteristicas` VALUES (10033,1),(20257,1),(22545,1),(28689,1),(64065,1),(10033,4),(22545,4),(64065,4),(10033,5),(20257,5),(28689,6),(64065,6),(10033,7),(22545,7),(22545,8),(20257,9),(28689,9),(22545,10),(28689,10),(64065,10),(22545,11),(10033,12),(64065,12),(22545,13),(28689,13),(22545,14);
 /*!40000 ALTER TABLE `tanques_caracteristicas` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `tipos`
+--
+
+DROP TABLE IF EXISTS `tipos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tipos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(17) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipos`
+--
+
+LOCK TABLES `tipos` WRITE;
+/*!40000 ALTER TABLE `tipos` DISABLE KEYS */;
+INSERT INTO `tipos` VALUES (1,'Árbol tecnológico'),(2,'Premium'),(3,'Coleccionista');
+/*!40000 ALTER TABLE `tipos` ENABLE KEYS */;
+UNLOCK TABLES;
 SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -115,4 +224,4 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-08-02 11:40:16
+-- Dump completed on 2026-08-21 22:06:14
